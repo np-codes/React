dae3132b43b2597623d821ad08158a2c505e3cdb
np-codes <nish632002.patel@gmail.com>
2024-06-03 20:29:49 -0400

    Add files via upload

---
+++ ep-2/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    [React.createElement(
+        "div",
+        {id:"child1"},
+        [React.createElement(
+            "h1",
+            {},
+            "This is first h1 tag in c1"
+        ),
+        React.createElement(
+            "h1",
+            {},
+            "This is second h1 tag in c1"
+        )]
+    ),
+    React.createElement(
+        "div",
+        {id:"child2"},
+        [React.createElement(
+            "h2",
+            {},
+            "This is first h1 tag in c2 "
+        ),
+        React.createElement(
+            "h2",
+            {},
+            "This is second h1 tag in c2 "
+        )]
+    )] 
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
